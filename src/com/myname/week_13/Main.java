a7b29c979154e2acf7112f84527fc197bc4d4e78
Kyiranes <evilmcchip@gmail.com>
2024-04-17 12:54:56 -0400

    Initial commit

---
+++ src/com/myname/week_13/Main.java
@@ -0,0 +1,53 @@
+package com.myname.week_13;
+
+class CheckingAccount {
+    public int balance;
+
+    public CheckingAccount(int initialBalance)
+    {
+        balance = initialBalance;
+    }
+
+    synchronized public int withdraw(int amount)
+    {
+        if (amount <= balance){
+            try {
+                Thread.sleep((int) (Math.random() * 200));
+            }
+            catch (InterruptedException ignored) {
+            }
+            balance -= amount;
+        }
+        return balance;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        CheckingAccount account = new CheckingAccount(100);
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                String name = Thread.currentThread().getName();
+                for (int i = 0; i < 10; i++) {
+                    if(account.balance == 0){
+                    break;
+                }
+                    System.out.println(name + " tries to withdraw $10, balance: " +
+                            account.withdraw(10));
+
+                }
+            }
+        };
+
+        Thread thdHusband = new Thread(r);
+        thdHusband.setName("Husband");
+
+        Thread thdWife = new Thread(r);
+        thdWife.setName("Wife");
+
+        thdHusband.start();
+        thdWife.start();
+    }
+}
